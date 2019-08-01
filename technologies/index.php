a0ea9af1d4c323938ed6550e0f8d2eb386f79238
Nathanaël Martel <nat@nathanaelmartel.net>
2019-08-01 11:24:20 +0200

    bug

--- technologies/index.php
+++ technologies/index.php
@@ -2,24 +2,24 @@
 <html lang="fr">
 
   <head>
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_head.php') ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'].'/_head.php'; ?>
     <title>Technologies - Nathanaël Martel - simplement Web</title>
 
   </head>
   <body>
 
 
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_header.php') ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'].'/_header.php'; ?>
 
         <section id="content">
 
             <h1>Technologies</h1>
             <p>L'apprentissage de nouvelles techniques, l'appropriation de nouvelles technologies font partie de mon métier.</p>
-            <p>Parmis celles que j'utilise, en voici quelques unes qui vous seront peut-être utiles : <a href="/technologies/api/">API et manipulation de données</a>, <a href="/techonologies/donnees-structurees/">Microdata et données structurées</a>, <a href="/technologies/responsive/">Responsive</a>.</p>
+            <p>Parmis celles que j'utilise, en voici quelques unes qui vous seront peut-être utiles : <a href="/technologies/api/">API et manipulation de données</a>, <a href="/technologies/donnees-structurees/">Microdata et données structurées</a>, <a href="/technologies/responsive/">Responsive</a>.</p>
 
         </section>
 
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_footer.php') ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'].'/_footer.php'; ?>
 
 
   </body>
