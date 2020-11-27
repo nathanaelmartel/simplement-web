faa8d537faccebc476fb47efb9eb233c69fdd6ac
Nathanaël Martel <nat@nathanaelmartel.net>
2020-11-27 13:53:35 +0100

    mentions légales

--- mentions-legales/index.php
+++ mentions-legales/index.php
@@ -14,20 +14,25 @@
         <section id="content">
 
             <h1>Mentions Légales</h1>
-            <p>Nathanaël Martel <br>
-            Entrepreneur salarié en <abbr title="Contrat d'Entrepreneur Salarié Associé">CESA</abbr> <a href="http://www.mozeika.fr/" target="_blank">Avec Mozeika</a><br>
+
+            <p>Nathanaël Martel, entrepreneur salarié en <abbr title="Contrat d'Entrepreneur Salarié Associé">CESA</abbr> dans la <abbr title="Coopérative d'Activités et d'Entrepreneurs">CAE</abbr> <a href="http://www.mozeika.fr/" target="_blank">Avec Mozeika</a><br>
+
+            <h2>Avec Mozeika</h2>
+            Société coopérative et participative par actions simplifiée à capital variable<br>
             66 boulevard François Grosso<br>
             06000 Nice <br>
             SIRET : <a href="https://entreprise.data.gouv.fr/sirene/82989996200023" target="_blank">829 899 962 00023</a> <br>
             N° TVA Intracommunautaire : FR25 829 899 962
           </p>
 
-            <p>Responsable de rédaction et de publication : Nathanaël Martel <br>
-
+            <h2>Responsable de rédaction et de publication</h2>
+            <p>Nathanaël Martel <br>
             Tel : <a class="tel" href="tel:+33781948086">07 81 94 80 86</a><br>
-            mail : <a href="mailto:nathanael@simplement-web.com">nathanael@simplement-web.com</a></p>
+            mail : <a href="mailto:nathanael@simplement-web.com">nathanael@simplement-web.com</a>
+          </p>
 
-            <p>Hébergeur : ONLINE SAS <br>BP 438 <br>75366 PARIS CEDEX 08</p>
+              <h2>Hébergeur</h2>
+            <p>ONLINE SAS <br>BP 438 <br>75366 PARIS CEDEX 08</p>
 
         </section>
 
